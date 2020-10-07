5c06c363cd53c7f72ce71a9ce7fe710eb89fb41e
ArifComek <acomek@gmail.com>
2020-10-07 21:45:30 +0200

    commit suer story 11

--- src/test/java/add_file_here/Arif.java
+++ src/test/java/add_file_here/Arif.java
@@ -1,4 +1,7 @@
 package add_file_here;
 
 public class Arif {
+    public static void main(String[] args) {
+        System.out.println("working hard..");
+    }
 }
--- src/test/java/add_file_here/HiAkbar.java
+++ src/test/java/add_file_here/HiAkbar.java
@@ -1,4 +1,7 @@
 package add_file_here;
 
 public class HiAkbar {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
 }
